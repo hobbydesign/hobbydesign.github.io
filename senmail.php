fd0054c3b23c8ac1c253b6c57d9ac806f1763336
Ellis Berner <eberner@gmail.com>
2013-07-08 17:55:33 -0700

    Remove unnecessary stuff

--- senmail.php
+++
@@ -1,7 +0,0 @@
-<?
-$email = $_GET['form_email'] ;
-$message = $_GET['form_message'] ;
-mail( "eberner@gmail.com", "[getahobbydesign.com]", $message, "From: $email" );
-print "Congratulations your email has been sent";
-?>
-
